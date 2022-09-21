e7b806a4b70faf7d27c505d05d8f243fb052a1ea
realain <wowwow6994@gmail.com>
2022-09-21 16:34:32 +0900

    commit test

--- src/main/java/hello/board/service/MemberService.java
+++ src/main/java/hello/board/service/MemberService.java
@@ -3,10 +3,12 @@ package hello.board.service;
 import hello.board.domain.Member;
 import hello.board.repository.MemberRepository;
 import hello.board.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
